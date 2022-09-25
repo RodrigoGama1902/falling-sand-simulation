82d800e748c4815df943f387facca2355e51af87
RodrigoGama <rodrigo1902.rb@gmail.com>
2022-09-25 13:16:43 -0300

    Better FPS counter

--- src/app/AppUtils.cpp
+++ src/app/AppUtils.cpp
@@ -1,10 +1,25 @@
 #include "AppUtils.h"
+#include "SDL2/SDL.h"
 #include <iostream>
 #include <iomanip>
 #include <ios>
 
+static uint32_t maxFPS = 0;
+static uint32_t minFPS = -1;
+
 void DisplayFPS(uint32_t frameTime)
 {
-    int fps = (frameTime > 0) ? 1000.0f / frameTime : 0.0f;
-    std::cout << "\r" << std::setw(3) << fps << " FPS" << std::flush;
+    if (SDL_GetTicks() > 1000) // Starting FPS counter after app stabilization
+    {
+        uint32_t fps = (frameTime > 0) ? 1000.0f / frameTime : 0.0f;
+
+        if (fps > maxFPS)
+            maxFPS = fps;
+        if (fps < minFPS)
+            minFPS = fps;
+
+        std::cout << "\r" << std::setw(3) << fps << " FPS"
+                  << " | " << std::setw(3) << maxFPS << " Max FPS"
+                  << " | " << std::setw(3) << minFPS << " Min FPS" << std::flush;
+    }
 }
