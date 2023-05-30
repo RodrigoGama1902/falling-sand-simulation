0b5fd95b0957fd364a0f6873d28436343cb8bf08
Rodrigo Gama <rodrigo1902.rb@gmail.com>
2023-05-30 00:04:02 -0300

    Updated readme.md

--- readme.md
+++ readme.md
@@ -2,15 +2,31 @@
 
 This is a C++ project that implements a Falling Sand Simulation using the SDL2 library. The simulation allows the user to interact with different elements such as sand, water, honey, and concrete.
 
+<div align="center">
+  <img src="docs/main.gif" alt="Main">
+</div>
+
 ## Features
 
 - **Debug Mode**: The application can be executed in a lower resolution debug mode. In this mode, the user can fast-forward frame by frame by pressing the left arrow key.
+
+<div align="center">
+  <img src="docs/debug.gif" alt="Main">
+</div>
 
 - **Elements**: The user can change between elements by pressing keys 1, 2, 3, and 4. The available elements are:
   - Sand
   - Water
   - Honey
   - Concrete
+
+<div align="center">
+  <img src="docs/all_elements.gif" alt="Main">
+</div>
+
+<div align="center">
+  <img src="docs/viscosity.gif" alt="Main">
+</div>
 
 ## TODO List
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "app/App.h"
 
-const bool DEBUG_MODE = true;
+const bool DEBUG_MODE = false;
 
 int main(int argc, const char *argv[])
 {
@@ -12,36 +12,3 @@ int main(int argc, const char *argv[])
     return 0;
 }
 
-//------------------------------------------------
-// TODO
-//------------------------------------------------
-
-// ELEMENTS ----------------------------
-
-// Add Gas Element
-
-// SIMULATION ------------------------------------
-
-// Add gravity
-
-// OPTIMIZATION ---------------------------------
-
-// Do not use vector
-// Do not use new keyword (use stack instead)
-
-// RENDER ----------------------------------------
-
-// Liquid color fade in is changing green only, refactor to change all colors, and make it fade in with different speeds
-
-// BUGS ------------------------------------------
-
-// When drawing, if draw out of the app screen, it crashes for some reason
-
-// FOR THE FUTURE -------------------------------
-
-// Add HSV color changes
-// Add a way to save and load grids
-// Add a way to create custom elements
-// mix different fluid elements, to get a new one with different color
-// Add post processing methods
-// Create clone points, when the space bar is pressed, a spawn of this element will be created
